fd9d9a8bdb1bed7ce82c9550ec5232886093112d
Hunter <hunter@drpanda.com>
2020-07-07 21:09:51 +0800

    new project

---
+++ Assets/DontDestroyScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyScript : MonoBehaviour
+{
+    public void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    // Use this for initialization
+
+    void Start()
+    {
+        //UmengGameAnalytics.instance.Init();
+        //AdsController.instance.Init();
+
+        admobdemo.Instance.initAdmob();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnDestroy()
+    {
+        //UmengGameAnalytics.instance.OnDestroy();
+        //AdsController.instance.OnDestroy();
+    }
+}
